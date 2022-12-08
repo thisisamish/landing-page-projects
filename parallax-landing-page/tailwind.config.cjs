88deffc45d0bbca11776ee519a1b20d0b099612e
Amish Verma <amishhverma@gmail.com>
2022-12-08 23:58:47 +0530

    Add Parallax Landing Page project with basic setup

---
+++ parallax-landing-page/tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./main.js"],
+  theme: {
+    extend: {
+      colors: {
+        bkg: "#171819",
+        muted: "#C9CBCF",
+        accent: "#66CCC1",
+      },
+    },
+  },
+  plugins: [],
+};
